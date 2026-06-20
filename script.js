1e6d2aad32ce55a325f03ec26fddecbb47aad7cd
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-06-20 22:38:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // write js code here if required
+const timer = document.getElementById("timer");
+
+function updateTimer() {
+	const now = new Date();
+
+	timer.textContent = now.toLocaleString();
+}
+
+setInterval(updateTimer, 1000);
